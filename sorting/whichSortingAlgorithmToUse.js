2c2b3cf24d8f1e15fceacea9ffeb33e278a5720d
bo98117 <bosin98117@gmail.com>
2022-08-31 19:56:28 -0700

    Quick Sort

---
+++ sorting/whichSortingAlgorithmToUse.js
@@ -0,0 +1,31 @@
+//#1 - Sort 10 schools around your house by distance:
+insertion sort
+input is only 10 which is very small
+space complexity of O(1)
+10 schools are nearly sorted or sorted
+
+//#2 - eBay sorts listings by the current Bid amount:
+radix or counting sort
+bid will always be numbers
+a bid is usually between $1 - $100 
+bid is fixed length
+
+//#3 - Sport scores on ESPN
+Quick sort
+
+
+//#4 - Massive database (can't fit all into memory) needs to sort through past year's user data
+Merge Sort
+
+//#5 - Almost sorted Udemy review data needs to update and add 2 new reviews
+Insertion Sort
+
+//#6 - Temperature Records for the past 50 years in Canada
+radix or counting Sort
+Quick sort if decimal places
+
+//#7 - Large user name database needs to be sorted. Data is very random.
+Quick sort
+
+//#8 - You want to teach sorting
+Bubble sort
