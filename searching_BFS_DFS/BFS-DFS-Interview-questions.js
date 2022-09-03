420402af0cc0582dc8e8145f1c55d334de71c5cc
bo98117 <bosin98117@gmail.com>
2022-09-03 07:43:32 -0700

    BFS DFS

---
+++ searching_BFS_DFS/BFS-DFS-Interview-questions.js
@@ -0,0 +1,19 @@
+//If you know a solution is not far from the root of the tree:
+BFS - it starts searching the closest node to the parent first
+
+//If the tree is very deep and solutions are rare, 
+BFS
+DFS - we use recursion in DFS which will take long time because the tree is very deep and the solutions are rare
+
+//If the tree is very wide: - meaning there are a ton of nodes underneath each parent
+DFS
+BFS will need too much memory because it needs to keep track of the nodes at each level
+
+//If solutions are frequent but located deep in the tree
+DFS
+
+//determining whether a path exists between two nodes
+DFS
+
+//Finding the shortest path
+BFS
