25ffe16d47f0d9ceb7eb26f54e86b9437ea41aa1
bo98117 <bosin98117@gmail.com>
2022-09-04 07:42:59 -0700

    Depth First Search using Pre, Post, and In order

--- searching_BFS_DFS/InPrePostOrder.js
+++ searching_BFS_DFS/InPrePostOrder.js
@@ -9,12 +9,10 @@
 BFS[9, 4, 20, 1, 6, 15, 170]
 DFS[9, 4, 1, 6, 20, 15, 170]
 
+InOrder - [1, 4, 6, 9, 15, 20, 170]
+PreOrder - [9, 4, 1, 6, 20, 15, 170]    // very useful in recreating a tree becaue it is ordered
+PostOrder - [1, 6, 4, 15, 170, 20, 9]
 
-
-
-InOrder - []
-PreOrder - []
-PostOrder - []
 
 
 
