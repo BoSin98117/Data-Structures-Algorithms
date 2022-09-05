52e3ed39d40f1aa5cccf429676d919772fee82a0
bo98117 <bosin98117@gmail.com>
2022-09-05 08:36:45 -0700

    Djikstra Bellman-Ford

--- searching_BFS_DFS/InPrePostOrder.js
+++ searching_BFS_DFS/InPrePostOrder.js
@@ -6,12 +6,39 @@
 //              9
 //      4               20
 //   1     6       15       170
-BFS[9, 4, 20, 1, 6, 15, 170]
-DFS[9, 4, 1, 6, 20, 15, 170]
+// BFS[9, 4, 20, 1, 6, 15, 170]
+// DFS[9, 4, 1, 6, 20, 15, 170]
 
-InOrder - [1, 4, 6, 9, 15, 20, 170]
-PreOrder - [9, 4, 1, 6, 20, 15, 170]    // very useful in recreating a tree becaue it is ordered
-PostOrder - [1, 6, 4, 15, 170, 20, 9]
+// InOrder - [1, 4, 6, 9, 15, 20, 170]
+// PreOrder - [9, 4, 1, 6, 20, 15, 170]    // very useful in recreating a tree becaue it is ordered
+// PostOrder - [1, 6, 4, 15, 170, 20, 9]
+
+
+// InOrder - root node is in the middle
+// PreOrder - root node is in the beginning
+// PostOrder - root node is at the end
+
+
+
+var isPalindrome = function (x) {
+    let str = x;
+    let start = 0;
+    let end = str.length - 1;
+    while (start < end) {
+        if (str.charAt(start++) != str.charAt(end--)) {
+            return false;
+        }
+        return true;
+    }
+}
+
+console.log(isPalindrome(121));
+
+
+
+
+
+
 
 
 
